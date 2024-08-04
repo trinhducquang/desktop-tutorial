3c72dbedc1b1668b72f2978454a0fca457110e8a
minhAptechGitHub <minh.dd.2417@aptechlearning.edu.vn>
2024-08-04 14:10:31 +0700

    chuyển tệp admin ra ngoài

---
+++ client/src/Admin/AdminConfig.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminConfig = {
+    "url" :"http://localhost/project1%20-%2030-7/desktop-tutorial/client/src/Admin/"
+}
+
+export default AdminConfig;
--- client/src/components/Admin/AdminConfig.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminConfig = {
-    "url" :"http://localhost/project1%20-%2030-7/desktop-tutorial/client/src/components/Admin/"
-}
-
-export default AdminConfig;
