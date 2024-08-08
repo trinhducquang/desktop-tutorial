573091bae0ce701aa8c2f7a4676a48d3b885f13e
minhAptechGitHub <minh.dd.2417@aptechlearning.edu.vn>
2024-08-09 00:02:27 +0700

    xong đổ dữ liệu trên cart

--- client/src/components/Carousel/Carousel2.jsx
+++ client/src/components/Carousel/Carousel2.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import './Carousel2.scss';
+import AdminConfig from '../../Admin/AdminConfig';
 
 const Carousel = ({ media, title }) => {
+    const { url } = AdminConfig;
     const settings = {
         infinite: true,
         speed: 500,
@@ -23,11 +25,92 @@ const Carousel = ({ media, title }) => {
         ],
     };
 
+
+    const [products, setProducts] = useState([])
+
+    const fetchData = async (url) => {
+        try {
+            let resp = await fetch(`${url}AdminProduct.php`, {
+                method: 'GET',
+                headers: {
+                    'X-React-File-Name': 'AdminProduct.jsx'
+                }
+            });
+
+            if (!resp.ok) {
+                throw new Error('Failed to fetch product data.');
+            }
+
+            let data = await resp.json();
+            // console.log(data);
+            setProducts(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(url);
+        fetchImage();
+        fetchProductAttri();
+    }, [url]);
+
+    const [productAttributes, setPorductAttributes] = useState([]);
+
+    const fetchProductAttri = async () => {
+        try {
+            let response = await fetch(`${url}AdminProduct.php`, {
+                headers: {
+                    'X-React-File-Name': 'AdminProductAttri.jsx'
+                }
+            });
+            if (!response.ok) {
+                console.log(response);
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            // console.log(data);
+            setPorductAttributes(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const [images, setImages] = useState([]);
+
+    const fetchImage = async () => {
+        try {
+            const response = await fetch(`${url}AdminProduct.php`, {
+                headers: {
+                    'X-React-File-Name': 'AdminImage.jsx'
+                }
+            })
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setImages(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const groupedImages = images.reduce((acc, img) => {
+        // if (img.product_id === products.id) {
+        if (!acc[img.product_id]) {
+            acc[img.product_id] = [];
+        }
+        acc[img.product_id].push(img);
+        // }
+        return acc;
+    }, {});
+
+
     return (
         <div className='carousel-container'>
             <p className='carousel-title'>{title}</p>
             <Slider {...settings} className='carousel-2-container'>
-                {media.map((item, index) => (
+                {/* {media.map((item, index) => (
                     <div key={index} className="carousel-2-item">
                         <img src={item.src} alt={`slide ${index}`} />
                         <div className='carousel-2-item-1'>
@@ -38,7 +121,43 @@ const Carousel = ({ media, title }) => {
                             </div>
                         </div>
                     </div>
-                ))}
+                ))} */}
+                {
+                    products.map((product) => (
+                        <div className="carousel-2-item" key={product.id}>
+                            {
+                                Object.entries(groupedImages)
+                                    .filter(([productId, imageList]) => productId === product.id)
+                                    .map(([productId, imageList]) => {
+                                        const [image1, image2] = imageList.slice(0, 2);
+
+                                        return (
+                                            // <div key={productId}>
+                                                image1 && (
+                                                    <img
+                                                        src={image1.image}
+                                                        alt={`Product ${productId} - Image 1`}
+                                                        
+                                                    />
+                                                )
+                                            // </div>
+                                        );
+                                    })
+                            }
+
+                            <div className='carousel-2-item-1'>
+                                <span className='item-caption'>{product.name}</span>
+                                <span className='item-price'>${product.price}</span>
+                                <div>
+                                    <button className='add-to-cart-button'>Add to Cart</button>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    ))
+                }
+
             </Slider>
         </div>
     );
