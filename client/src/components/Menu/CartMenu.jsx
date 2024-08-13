eb7dc15706eb90ed1a81a3f85069cdb5f1ca14ea
minhAptechGitHub <minh.dd.2417@aptechlearning.edu.vn>
2024-08-13 17:55:09 +0700

    sua cong tru quanity

--- client/src/components/Menu/CartMenu.jsx
+++ client/src/components/Menu/CartMenu.jsx
@@ -176,9 +176,9 @@ const CartMenu = ({ isCartOpen, handleCloseCart }) => {
                 </div>
               </div>
               <div>
-                <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
-                <button>{item.quantity}</button>
-                <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
+                <button type='button' onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
+                <button disabled>{item.quantity}</button>
+                <button type='button' onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
               </div>
             </div>
           ))}
