7925225a16bf9c7494db95e37be2c1ac3bbf5313
trinhducquang <quang.td.2430@aptechlearning.edu.vn>
2024-08-15 17:21:30 +0700

    up

--- client/src/Admin/Admin.jsx
+++ client/src/Admin/Admin.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import useScrollTranslate from '../Hooks/useScrollTranslate';
 import './Admin.scss'
 
 const Admin = () => {
 
+    const scrollRef = useScrollTranslate(9000);
+
     return (
             <div className="admin-container">
                 <div><p>Admin</p></div>
-                <div className="admin-item">
+                <div ref={scrollRef} className="admin-item">
                     <Link to="/Admin/product">Product</Link>
                     <Link to="/Admin/attri">Attribute</Link>
                     <Link to="/Admin/user">Users</Link>
