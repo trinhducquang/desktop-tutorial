588be5e450b4db8227616db858ef559140737d4c
trinhducquang <quang.td.2430@aptechlearning.edu.vn>
2024-08-26 13:24:17 +0700

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -70,37 +70,37 @@ const App = () => {
 
           <Route path='/Admin' element={<Admin />} />
 
-          <Route path='/Admin/product' element={<AdminProduct />}></Route>
-          <Route path='/Admin/product/edit/:id' element={<AdminEdit />}></Route>
-          <Route path='/Admin/product/new' element={<AdminNew />}></Route>
+          <Route path='/Admin/product' element={<AdminProduct />}/>
+          <Route path='/Admin/product/edit/:id' element={<AdminEdit />}/>
+          <Route path='/Admin/product/new' element={<AdminNew />}/>
 
-          <Route path='/Admin/attri' element={<AdminAttribute />}></Route>
-          <Route path='/Admin/attri/edit/:id' element={<AdminEdit2 />}></Route>
-          <Route path='/Admin/attri/new' element={<AdminNew2 />}></Route>
+          <Route path='/Admin/attri' element={<AdminAttribute />}/>
+          <Route path='/Admin/attri/edit/:id' element={<AdminEdit2 />}/>
+          <Route path='/Admin/attri/new' element={<AdminNew2 />}/>
 
-          <Route path='/Admin/attri_value/:id' element={<AdminAttriValue />}></Route>
-          <Route path='/Admin/attri_value/edit/:id' element={<AdminEdit3 />}></Route>
-          <Route path='/Admin/attri_value/new/:id' element={<AdminNew3 />}></Route>
+          <Route path='/Admin/attri_value/:id' element={<AdminAttriValue />}/>
+          <Route path='/Admin/attri_value/edit/:id' element={<AdminEdit3 />}/>
+          <Route path='/Admin/attri_value/new/:id' element={<AdminNew3 />}/>
 
-          <Route path='/Admin/user/' element={<AdminUsers />}></Route>
-          <Route path='/Admin/user/edit/:id' element={<AdminEdit4 />}></Route>
-          <Route path='/Admin/user/new/' element={<AdminNew4 />}></Route>
+          <Route path='/Admin/user/' element={<AdminUsers />}/>
+          <Route path='/Admin/user/edit/:id' element={<AdminEdit4 />}/>
+          <Route path='/Admin/user/new/' element={<AdminNew4 />}/>
 
-          <Route path='/Admin/order' element={<AdminOrders />}></Route>
-          <Route path='/Admin/order/edit/:id' element={<AdminEdit5 />}></Route>
+          <Route path='/Admin/order' element={<AdminOrders />}/>
+          <Route path='/Admin/order/edit/:id' element={<AdminEdit5 />}/>
           {/* <Route path='/Admin/order/new' element={<AdminNew5/>}></Route> */}
 
-          <Route path='/Admin/order_detail/:id' element={<AdminOrderDetail />}></Route>
-          <Route path='/Admin/order_detail/edit/:id' element={<AdminEdit6 />}></Route>
+          <Route path='/Admin/order_detail/:id' element={<AdminOrderDetail />}/>
+          <Route path='/Admin/order_detail/edit/:id' element={<AdminEdit6 />}/>
           {/* <Route path='/Admin/order_detail/new/:id' element={<AdminNew6/>}></Route> */}
 
-          <Route path='/Admin/video' element={<AdminVideo />}></Route>
-          <Route path='/Admin/video/edit/:id' element={<AdminEdit7 />}></Route>
-          <Route path='/Admin/video/new' element={<AdminNew7 />}></Route>
+          <Route path='/Admin/video' element={<AdminVideo />}/>
+          <Route path='/Admin/video/edit/:id' element={<AdminEdit7 />}/>
+          <Route path='/Admin/video/new' element={<AdminNew7 />}/>
 
-          <Route path='/Admin/image/:id' element={<AdminImage />}></Route>
-          <Route path='/Admin/image/edit/:id' element={<AdminEdit8 />}></Route>
-          <Route path='/Admin/image/new/:id' element={<AdminNew8 />}></Route>
+          <Route path='/Admin/image/:id' element={<AdminImage />}/>
+          <Route path='/Admin/image/edit/:id' element={<AdminEdit8 />}/>
+          <Route path='/Admin/image/new/:id' element={<AdminNew8 />}/>
 
         </Routes>
 
